14f9979eeab2c368514921bfbcda4e4a9b8696f7
aslavicek21 <aslavicek21@foi.hr>
2024-01-14 00:54:09 +0100

    Added weather module with working API. Edited CSS for monitor orientation.

--- js/server.js
+++ js/server.js
@@ -10,6 +10,43 @@ function Server() {
     const port = 5200;
     let server = null;
 
+    this.fetchWeatherData = async function (ws) {
+        let weatherData = "";
+
+        fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Vara%C5%BEdin?unitGroup=metric&key=CPJEFE4NAQPHJQ9DCK8C6Z5HK&contentType=json", {
+        method: 'GET', 
+        headers: {},    
+        }).then(response => {
+        if (!response.ok) {
+            throw response; //check the http response code and if isn't ok then throw the response as an error
+        }
+      
+        return response.json(); //parse the result as JSON
+
+        }).then(response => {
+        // response now contains parsed JSON ready for use
+        // let weather = "";
+        weatherData = response.address + "," + 
+                    response.days[0].datetime + "," + 
+                    response.currentConditions.temp + "," + 
+                    response.currentConditions.humidity + "," + 
+                    response.days[0].description + "," + 
+                    response.alerts[0].description;
+        console.log(weatherData);
+        ws.send("weatherModule$$" + weatherData);
+
+        }).catch((errorResponse) => {
+        if (errorResponse.text) { //additional error information
+            errorResponse.text().then( errorMessage => {
+            //errorMessage now returns the response body which includes the full error message
+            })
+        } else {
+            //no additional error information 
+        } 
+        });
+
+    }
+
     this.open = async function () {
         server = http.Server(app);
         
@@ -31,6 +68,9 @@ function Server() {
                     modules.push(string.substring(1));
                 }
                 else if(string.startsWith("#")) {
+                    if (string.substring(1) == "weatherModule") {
+                        this.fetchWeatherData(ws);
+                    }
                     // if string.substring(1) == moduleName then call function for fetching online API data
                     // send data with ws.send("moduleName$$DATA_HERE")
                 }
